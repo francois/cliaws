7b5b743bcbbd63b0a060c1025c7e102ad9b10fac
François Beausoleil <francois@teksol.info>
2008-04-12 07:43:39 -0400

    Initial implementation of S3 client.

---
+++ lib/cliaws.rb
@@ -0,0 +1,17 @@
+$:.unshift(File.dirname(__FILE__)) unless $:.include?(File.dirname(__FILE__)) || $:.include?(File.expand_path(File.dirname(__FILE__)))
+
+require "cliaws/s3"
+
+module Cliaws
+  def self.access_key_id
+    ENV["AWS_ACCESS_KEY_ID"]
+  end
+
+  def self.secret_access_key
+    ENV["AWS_SECRET_ACCESS_KEY"]
+  end
+
+  def self.s3
+    @@s3 ||= Cliaws::S3.new(access_key_id, secret_access_key)
+  end
+end
---
+++ lib/cliaws/s3.rb
@@ -0,0 +1,46 @@
+require "right_aws"
+require "activesupport"
+
+module Cliaws
+  class S3
+    def initialize(access_key_id, secret_access_key)
+      @s3 = RightAws::S3.new(access_key_id, secret_access_key, :logger => Logger.new("/dev/null"))
+    end
+
+    def list(glob)
+      bucket, path = bucket_and_key_name(glob)
+      options = Hash.new
+      options["prefix"] = path unless path.blank?
+      bucket.keys(options).map(&:full_name)
+    end
+
+    def get(s3_object, dest)
+      bucket, keyname = bucket_and_key_name(s3_object)
+      dest.write(bucket.get(keyname))
+    end
+
+    def head(s3_object)
+      bucket, keyname = bucket_and_key_name(s3_object)
+      key = bucket.key(keyname, true)
+      headers = key.headers
+      puts headers.merge(key.meta_headers).to_yaml
+    end
+
+    def put(source, s3_object)
+      bucket, keyname = bucket_and_key_name(s3_object)
+      bucket.put(keyname, source)
+    end
+
+    def rm(s3_object)
+      bucket, keyname = bucket_and_key_name(s3_object)
+      bucket.key(keyname).delete
+    end
+
+    protected
+    def bucket_and_key_name(full_name)
+      bucket_name, path = full_name.split("/", 2)
+      bucket = @s3.bucket(bucket_name, false)
+      [bucket, path]
+    end
+  end
+end
